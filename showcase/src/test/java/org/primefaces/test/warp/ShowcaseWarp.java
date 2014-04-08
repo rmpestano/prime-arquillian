7631bbc53fc43847cee9a5adbf85af708f6f5264
rmpestano <rmpestano@gmail.com>
2014-04-08 00:37:33 -0300

    more warp config

--- showcase/src/test/java/org/primefaces/test/warp/ShowcaseWarp.java
+++ showcase/src/test/java/org/primefaces/test/warp/ShowcaseWarp.java
@@ -12,24 +12,25 @@ import org.junit.Test;
 import org.primefaces.test.pages.exceptionhandler.ExceptionHandlerHome;
 
 import javax.faces.context.FacesContext;
+import javax.faces.event.ExceptionQueuedEvent;
 
 /**
  * Created by rmpestano on 4/7/14.
  */
 
-public class ShowcaseWarp extends BaseWarp  {
+public class ShowcaseWarp extends BaseWarp {
 
 
     @Test
     @InSequence(0)
-    public void init(){
-        browser.get(baseUrl +"index.jsp");
+    public void init() {
+        browser.get(baseUrl + "index.jsp");
     }
 
 
     @Test
     @InSequence(1)
-    public void shouldThrowViewExpired(@InitialPage final ExceptionHandlerHome exceptionHandlerHome){
+    public void shouldThrowViewExpired(@InitialPage final ExceptionHandlerHome exceptionHandlerHome) {
         Warp.initiate(new Activity() {
             public void perform() {
                 exceptionHandlerHome.fireViewExpired();
@@ -38,14 +39,60 @@ public class ShowcaseWarp extends BaseWarp  {
         }).inspect(new Inspection() {
             private static final long serialVersionUID = 1L;
 
-            @AfterPhase(value = Phase.RENDER_RESPONSE)
+
+            @AfterPhase(value = Phase.APPLY_REQUEST_VALUES)
             public void verify(@ArquillianResource FacesContext context) {
-                  context.getMessageList();
-                  context.getExceptionHandler().getHandledExceptionQueuedEvent();
+                System.out.println("VERIFY");
+                System.out.println(context.getMessageList());
+                System.out.println(context.getAttributes());
+                System.out.println(context.getExceptionHandler().getHandledExceptionQueuedEvent());
+                for (ExceptionQueuedEvent exceptionQueuedEvent : context.getExceptionHandler().getHandledExceptionQueuedEvents()) {
+                    System.out.println(exceptionQueuedEvent);
+                }
             }
 
+            @AfterPhase(value = Phase.RENDER_RESPONSE)
+            public void RENDER(@ArquillianResource FacesContext context) {
+                System.out.println("RENDER");
+                System.out.println(context.getMessageList());
+                System.out.println(context.getAttributes());
+                System.out.println(context.getExceptionHandler().getHandledExceptionQueuedEvent());
+                for (ExceptionQueuedEvent exceptionQueuedEvent : context.getExceptionHandler().getHandledExceptionQueuedEvents()) {
+                    System.out.println(exceptionQueuedEvent);
+                }
+
+
+            }
+
+            @AfterPhase(value = Phase.UPDATE_MODEL_VALUES)
+            public void update(@ArquillianResource FacesContext context) {
+                System.out.println("UPDATE");
+                System.out.println(context.getMessageList());
+                System.out.println(context.getAttributes());
+                System.out.println(context.getExceptionHandler().getHandledExceptionQueuedEvent());
+                for (ExceptionQueuedEvent exceptionQueuedEvent : context.getExceptionHandler().getHandledExceptionQueuedEvents()) {
+                    System.out.println(exceptionQueuedEvent);
+                }
+
+            }
+
+
+            @AfterPhase(value = Phase.INVOKE_APPLICATION)
+            public void invoke(@ArquillianResource FacesContext context) {
+                System.out.println("INVOKE");
+                System.out.println(context.getMessageList());
+                System.out.println(context.getAttributes());
+                System.out.println(context.getExceptionHandler().getHandledExceptionQueuedEvent());
+                for (ExceptionQueuedEvent exceptionQueuedEvent : context.getExceptionHandler().getHandledExceptionQueuedEvents()) {
+                    System.out.println(exceptionQueuedEvent);
+                }
+
+            }
         });
 
     }
 
+    ;
 }
+
+
